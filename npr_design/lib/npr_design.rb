0c99a0d5ff9a80601af699a329e0b38fab00e034
ElleeB <laura.trager@gmail.com>
2018-01-28 18:43:25 -0500

    Initial file setup

--- npr_design/lib/npr_design.rb
+++ npr_design/lib/npr_design.rb
@@ -1,5 +1,8 @@
+## This file will require all others
 require "npr_design/version"
+require_relative './cli.rb'
+require_relative './story.rb'
+require_relative './scraper.rb'
 
-module NprDesign
-  # Your code goes here...
-end
+require 'nokogiri'
+require 'open-uri'
