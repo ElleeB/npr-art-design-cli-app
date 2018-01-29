56d760de0b22e76bf4e86a137590630436c66317
ElleeB <laura.trager@gmail.com>
2018-01-29 14:17:54 -0500

    Update cli.rb list stories with index

--- npr_design/lib/cli.rb
+++ npr_design/lib/cli.rb
@@ -13,23 +13,23 @@ class NprDesign::CLI
     puts "Today's date: #{Date.today}"
     puts "Here are your weekly stories:"
     puts ""
+
+    menu = NprDesign::Story.all.each.with_index(1){|x, i| puts "#{i}. #{x}" }
+
     # Invite user to enter which story they'd like to learn more about
-      # Title
-      # Category
-      # Blurb
     puts "Please enter the corresponding number for your story of interest."
-
-    puts NprDesign::Story.all
-
+    story_details
   end
 
   def story_details
     # Gets input string-number corresponding to story
-
     input = gets.strip
+    puts "#{input} pow!"
     # Display story: Title, Date, Author, Text
     # Options: Quit, Return to Menu
-    
+  end
+
+  def valid?(input)
   end
 
   def goodbye
