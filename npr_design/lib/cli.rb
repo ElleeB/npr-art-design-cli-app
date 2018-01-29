513c3cf9329993f7fe8a7cb0a6c46a96ce551827
ElleeB <laura.trager@gmail.com>
2018-01-29 13:56:17 -0500

    Stub Story .all

--- npr_design/lib/cli.rb
+++ npr_design/lib/cli.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 class NprDesign::CLI
 
   def call
@@ -8,6 +10,7 @@ class NprDesign::CLI
   end
 
   def menu
+    puts "Today's date: #{Date.today}"
     puts "Here are your weekly stories:"
     puts ""
     # Invite user to enter which story they'd like to learn more about
@@ -15,16 +18,18 @@ class NprDesign::CLI
       # Category
       # Blurb
     puts "Please enter the corresponding number for your story of interest."
-    puts story_1.category = "Fine Art"
-    puts story_1.title = "I Love Art"
-    puts story_1.blurb = "Find out how cool art is. If you don't know, you better know. Art is the coolest."
-    puts story_2.category = "Design"
-    puts story_2.title = "Design Is Cool"
-    puts story_2.blurb = "You better read this article or you are dumb. This story is life changing, don't miss it, or else."
+
+    puts NprDesign::Story.all
+
+  end
+
+  def story_details
     # Gets input string-number corresponding to story
+
     input = gets.strip
     # Display story: Title, Date, Author, Text
     # Options: Quit, Return to Menu
+    
   end
 
   def goodbye
--- npr_design/lib/story.rb
+++ npr_design/lib/story.rb
@@ -1,5 +1,25 @@
 class NprDesign::Story
 
-  attr_accessor :title, :author, :category, :blurb, :text
+  attr_accessor :title, :author, :category, :blurb, :text, :date
+
+  @stories = []
+
+
+  def self.all
+
+    # @stories
+
+    story_1 = self.new
+    story_1.category = "Fine Art"
+    story_1.title = "I Love Art"
+    story_1.blurb = "Find out how cool art is. If you don't know, you better know. Art is the coolest."
+
+    story_2 = self.new
+    story_2.category = "Design"
+    story_2.title = "Design Is Cool"
+    story_2.blurb = "You better read this article or you are dumb. This story is life changing, don't miss it, or else."
+
+    [story_1, story_2]
+  end
 
 end
