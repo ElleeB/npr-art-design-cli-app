8c4a67b42a92aced9db7f33b6738863d5e0a81bb
ElleeB <laura.trager@gmail.com>
2018-01-29 15:03:36 -0500

    Trblshting cli.rb #story_details input == 'menu'

--- npr_design/lib/cli.rb
+++ npr_design/lib/cli.rb
@@ -1,7 +1,7 @@
 class NprDesign::CLI
 
   def call
-    puts "Welcome to Your NPR News: Art & Design"
+    puts "Welcome to Your NPR News: Art & Design Edition"
     puts ""
     menu
     story_details
@@ -18,28 +18,39 @@ class NprDesign::CLI
 
   def story_details
     # Invite user to enter which story they'd like to learn more about
-    puts "Please enter the corresponding number for your story of interest."
+    puts ""
+    puts "Please enter the corresponding number for your story of interest, 'menu' to return to the story menu, or 'exit' to exit the program."
 
     # Gets input string-number corresponding to story
     input = gets.strip
-    if valid?
-      if input > 0
-        #story_instance.text_display
-      elsif input == "menu"
-        menu
-      elsif input == "exit"
-        goodbye
-      end
-    else
-      puts "Please enter a valid number"
+
+    #if valid?
+
+    if input.to_i > 0
+      puts "Storytime!"#story_instance.text_display
+    elsif input == "menu"
+      call
+      #=> Today's date: 2018-01-29
+        # Here are your weekly stories:
+        # 1. #<NprDesign::Story:0x007f92d206da80>
+        # 2. #<NprDesign::Story:0x007f92d206d738>
+        # 3. #<NprDesign::Story:0x007f92d206c8b0>
+        # 4. #<NprDesign::Story:0x007f92d206c798>
+    elsif input == "exit"
+      puts goodbye
     end
+
+  # else
+  #   puts "Please enter a valid number"
+  # end
+
   end
 
   def valid?(input)
   end
 
   def goodbye
-    # Goodbye
+    "Goodbye! Thanks for reading."
   end
 
 end
