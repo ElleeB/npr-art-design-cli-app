f34146663bbf4b42df2c5413860f02b04b936467
ElleeB <laura.trager@gmail.com>
2018-01-29 12:13:14 -0500

    cli.rb update

---
+++ npr_design/lib/cli.rb
@@ -0,0 +1,14 @@
+class CLI
+
+  # Greet users
+  # Provide Menu
+  # Invite user to enter which story they'd like to learn more about
+    # Title
+    # Category
+    # Blurb
+  # Input number corresponding to story
+  # Get input and display story: Title, Date, Author, Text
+  # Options: Quit, Return to Menu
+  # Goodbye
+
+end
