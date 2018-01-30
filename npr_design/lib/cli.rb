6d769ccac3f279d25bd92bb6b86098e8e95c2b52
ElleeB <laura.trager@gmail.com>
2018-01-29 19:32:35 -0500

    Improved Story class initialization and save.

--- npr_design/lib/cli.rb
+++ npr_design/lib/cli.rb
@@ -13,29 +13,19 @@ class NprDesign::CLI
     puts ""
 
     NprDesign::Story.all.each.with_index(1){|x, i| puts "#{i}. #{x}"}
-
   end
 
   def story_details
     # Invite user to enter which story they'd like to learn more about
     puts ""
     puts "Please enter the corresponding number for your story of interest, 'menu' to return to the story menu, or 'exit' to exit the program."
-
     # Gets input string-number corresponding to story
     input = gets.strip
-
     #if valid?
-
     if input.to_i > 0
       puts "Storytime!"#story_instance.text_display
     elsif input == "menu"
-      call
-      #=> Today's date: 2018-01-29
-        # Here are your weekly stories:
-        # 1. #<NprDesign::Story:0x007f92d206da80>
-        # 2. #<NprDesign::Story:0x007f92d206d738>
-        # 3. #<NprDesign::Story:0x007f92d206c8b0>
-        # 4. #<NprDesign::Story:0x007f92d206c798>
+      menu
     elsif input == "exit"
       puts goodbye
     end
--- npr_design/lib/story.rb
+++ npr_design/lib/story.rb
@@ -2,10 +2,19 @@ class NprDesign::Story
 
   attr_accessor :title, :author, :category, :blurb, :text, :date
 
-  @stories = []
+  @@stories = []
 
-  def initialize
+  def initialize(title, author)
+    @title = title
+    @author = author
+    # @category = nil
+    # @blurb = nil
+    # @text = nil
+    # @date = nil
+  end
 
+  def save
+    @@stories << self
   end
 
   def self.create_from_collection
@@ -14,29 +23,8 @@ class NprDesign::Story
   def add_attributes
   end
 
-  def menu_display
-  end
-
-  def text_display
-  end
-
-
   def self.all
-
-    # @stories
-
-    story_1 = self.new
-    story_1.category = "Fine Art"
-    story_1.title = "I Love Art"
-    story_1.blurb = "Find out how cool art is. If you don't know, you better know. Art is the coolest."
-    @stories << story_1
-
-    story_2 = self.new
-    story_2.category = "Design"
-    story_2.title = "Design Is Cool"
-    story_2.blurb = "You better read this article or you are dumb. This story is life changing, don't miss it, or else."
-    @stories << story_2
-
+    @@stories
   end
 
 end
