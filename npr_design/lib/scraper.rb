ea16ac709b9ad48334207c3b32d419675d137b1d
ElleeB <laura.trager@gmail.com>
2018-01-30 15:59:13 -0500

    Update previous to fix scrape return

--- npr_design/lib/scraper.rb
+++ npr_design/lib/scraper.rb
@@ -1,7 +1,8 @@
 class NprDesign::Scraper
 
+  @stories_array = []
+
   def self.menu_scraper(menu_url)
-    @menu_hash = {:title => nil, :url => nil}
     # gets title, story.url
     doc = Nokogiri::HTML(open(menu_url))
     stories = doc.css("section#main-section")
@@ -9,15 +10,18 @@ class NprDesign::Scraper
       @stories = stories.css("h2.title a")
     end
     @stories.each do |x|
+      @menu_hash = {:title => nil, :url => nil}
       @menu_hash[:title] = "#{x.text}"
       @menu_hash[:url] = "#{x['href']}"
+      @stories_array << @menu_hash
     end
-    @menu_hash
+    @stories_array
   end
 
-  def self.story_scraper(menu_hash)
-    @story_hash = {:author => nil, :category => nil, :blurb => nil, :text => nil, :date => nil}
-    # gets text and additional story attributes
+  def self.story_scraper(stories_array)
+    # @story_hash = {:author => nil, :category => nil, :blurb => nil, :text => nil, :date => nil}
+    # # gets text and additional story attributes
+    # @menu_hash[:url]
   end
 
 end
