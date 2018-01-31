79157e9108847db87200b3f8a65a6ffd290f02f2
ElleeB <laura.trager@gmail.com>
2018-01-30 23:50:05 -0500

    Menu scraper complete

--- npr_design/lib/scraper.rb
+++ npr_design/lib/scraper.rb
@@ -2,31 +2,20 @@ class NprDesign::Scraper
 
   def self.menu_scraper(menu_url)
     @stories_array = []
-    # gets title, story.url
     doc = Nokogiri::HTML(open(menu_url))
     stories = doc.css("article")
     info = stories.css("div.item-info")
 
     info.each do |story|
-      @menu_hash = {:title => nil, :url => nil, :category => nil, :blurb => nil, :date => nil}
-      # @menu_hash[:title} = story.css("h2.title").text
-      # @menu_hash[:url} = story.at("p.teaser a")["href"]
-      # @menu_hash[:blurb] = story.css("p.teaser").text #minus the first however many characters to remove date
+      @menu_hash = {:title => nil, :url => nil, :category => nil, :blurb => nil}
+      @menu_hash[:title] = story.css("h2.title").text
+      @menu_hash[:url] = story.at("p.teaser a")["href"]
+      @menu_hash[:category] = story.css("h3.slug a").text
+      @menu_hash[:blurb] = story.css("p.teaser").text #minus the first however many characters to remove date
+      @stories_array << @menu_hash
     end
 
+    @stories_array
   end
-
-
-  # def self.story_scraper(stories_array)
-  #
-  #   # :text => nil,
-  #   # :author => nil
-  #   # gets text and additional story attributes
-  #
-  #   stories_array.each do |x|
-  #     doc = Nokogiri::HTML(open("https://www.npr.org/2018/01/28/581006370/at-one-nasa-lab-art-and-science-share-the-same-orbit"))
-  #     puts doc
-  #   end
-  # end
 
 end
--- npr_design/lib/story.rb
+++ npr_design/lib/story.rb
@@ -1,6 +1,6 @@
 class NprDesign::Story
 
-  attr_accessor :title, :author, :category, :blurb, :text, :date, :url
+  attr_accessor :title, :author, :category, :blurb, :text, :url
 
   @@stories = []
 
@@ -10,7 +10,7 @@ class NprDesign::Story
     @category = category
     @blurb = blurb
     @text = text
-    @date = date
+    @url = url
   end
 
   def save
@@ -18,14 +18,12 @@ class NprDesign::Story
   end
 
   def self.create_from_collection(stories_array)
-    stories_array.each do |story|
-      self.title = "#{story[:title]}"
-      puts self.title
-    end
+
   end
 
 
-  def add_attributes
+  def text_display(url)
+    #put this in Scrape class?
   end
 
   def self.all
