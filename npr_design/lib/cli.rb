43cade1028f972edc1de3445e3eb2f3c996147af
ElleeB <laura.trager@gmail.com>
2018-02-01 09:47:09 -0500

    Basic cli update -  working

--- npr_design/lib/cli.rb
+++ npr_design/lib/cli.rb
@@ -14,16 +14,26 @@ class NprDesign::CLI
   end
 
   def make_stories
+    menu_url = "https://www.npr.org/sections/art-design/" # is there a different way to do this?
+    stories_array = NprDesign::Scraper.menu_scraper(menu_url) # how to get url here
+    NprDesign::Story.create_from_collection(stories_array)
   end
 
   def add_attributes
+    NprDesign::Story.all.each do |story|
+      story.add_text_and_author
+    end
   end
 
   def menu
     puts "Today's date: #{Date.today}"
     puts "Here are your weekly stories:"
     puts ""
-    NprDesign::Story.all.each.with_index(1){|x, i| puts "#{i}. #{x}"}
+    NprDesign::Story.all.each.with_index(1) do |x, i| # display_menu_items
+      puts "#{i}. #{x.title}"
+      puts "#{x.category}" #some not listing category - do some not have a category?
+      puts "#{x.blurb}"
+    end
   end
 
   def display_menu_items
@@ -31,7 +41,7 @@ class NprDesign::CLI
   end
 
   def story_text
-    # Invite user to enter which story they'd like to learn more about
+    # Invite user to enter which story they'd like to learn more about and display text
     puts ""
     puts "Please enter the corresponding number for your story of interest, 'menu' to return to the story menu, or 'exit' to exit the program."
     # Gets input string-number corresponding to story
